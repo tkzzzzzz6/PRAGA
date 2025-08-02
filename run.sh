1284395872ee1acd5a7cd00ae44f607bac643594
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2025-08-02 10:19:29 +0800

    Update run.sh to correct paths for ground truth files in evaluation commands for Mouse Brain and Simulation datasets.

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ python cal_matrics.py --GT_path './Data/HLN/GT_labels.txt' --our_path './results
 ## Mouse Brain
 python main.py --file_fold './Data/Mouse_Brain/' --data_type 'Spatial-epigenome-transcriptome' --n_clusters 14 --init_k 14 --KNN_k 20 --RNA_weight 1 --ADT_weight 10 --vis_out_path 'results/MB.png' --txt_out_path 'results/MB.txt'
 # eval
-python cal_matrics.py --GT_path './Data/MB_cluster.txt' --our_path './results/MB.txt' --save_path './results/MB_metrics.txt'
+python cal_matrics.py --GT_path './Data/Mouse_Brain/MB_cluster.txt' --our_path './results/MB.txt' --save_path './results/MB_metrics.txt'
 
 ## Simulation
 python main.py --file_fold './Data/Simulation/' --data_type 'Simulation' --n_clusters 5 --init_k 5 --KNN_k 20 --vis_out_path 'results/Simulation.png' --txt_out_path 'results/Simulation.txt'
 # eval
-python cal_matrics.py --GT_path 'GT_1.txt' --our_path './results/Simulation.txt' --save_path './results/Simulation_metrics.txt'
+python cal_matrics.py --GT_path './Data/Simulation/GT.txt' --our_path './results/Simulation.txt' --save_path './results/Simulation_metrics.txt'
 
 
 
